e3ba891b3e4a45785b1f0d288761e50b606f564e
igor.goldobin@gmail.com <Phoenix88>
2016-01-30 10:23:06 +1000

    Added excluded files

---
+++ src/SSW.MusicStore.Data/AmbientDbContextLocator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.Entity;
+
+using SSW.MusicStore.Data.Interfaces;
+
+namespace SSW.MusicStore.Data
+{
+    public class AmbientDbContextLocator : IAmbientDbContextLocator
+    {
+        public TDbContext Get<TDbContext>() where TDbContext : DbContext
+        {
+            var ambientDbContextScope = DbContextScope.GetAmbientScope();
+            return ambientDbContextScope?.DbContexts.Get<TDbContext>();
+        }
+    }
+}
---
+++ src/SSW.MusicStore.Data/DbContextReadOnlyScope.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+
+using SSW.MusicStore.Data.Interfaces;
+
+namespace SSW.MusicStore.Data
+{
+    public class DbContextReadOnlyScope : IDbContextReadOnlyScope
+    {
+        private DbContextScope _internalScope;
+
+        public IDbContextCollection DbContexts { get { return _internalScope.DbContexts; } }
+
+        public DbContextReadOnlyScope(IDbContextFactory dbContextFactory = null)
+            : this(joiningOption: DbContextScopeOption.JoinExisting, isolationLevel: null, dbContextFactory: dbContextFactory)
+        { }
+
+        public DbContextReadOnlyScope(IsolationLevel isolationLevel, IDbContextFactory dbContextFactory = null)
+            : this(joiningOption: DbContextScopeOption.ForceCreateNew, isolationLevel: isolationLevel, dbContextFactory: dbContextFactory)
+        { }
+
+        public DbContextReadOnlyScope(DbContextScopeOption joiningOption, IsolationLevel? isolationLevel, IDbContextFactory dbContextFactory = null)
+        {
+            _internalScope = new DbContextScope(joiningOption: joiningOption, readOnly: true, isolationLevel: isolationLevel, dbContextFactory: dbContextFactory);
+        }
+
+        public void Dispose()
+        {
+            _internalScope.Dispose();
+        }
+    }
+}
