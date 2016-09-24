5e43c0db302fa40c0443cbd8c81a3d4a8c859c93
Igor Goldobin <igor.goldobin@gmail.com>
2016-09-24 16:58:33 +1000

    Remoevd dependency on DataOnion.Core for Data project

--- src/SSW.MusicStore.Data/Repositories/Repositories.cs
+++ src/SSW.MusicStore.Data/Repositories/Repositories.cs
@@ -1,62 +1,62 @@
-﻿using SSW.DataOnion.Core;
-using SSW.DataOnion.Interfaces;
-using SSW.MusicStore.Data.Entities;
+﻿//using SSW.DataOnion.Core;
+//using SSW.DataOnion.Interfaces;
+//using SSW.MusicStore.Data.Entities;
 
-namespace SSW.MusicStore.Data.Repositories
-{
-    public class AlbumRepository : BaseRepository<Album, MusicStoreContext>
-    {
-        public AlbumRepository(IAmbientDbContextLocator dbContextScopeLocator)
-            : base(dbContextScopeLocator)
-        {
-        }
-    }
+//namespace SSW.MusicStore.Data.Repositories
+//{
+//    public class AlbumRepository : BaseRepository<Album, MusicStoreContext>
+//    {
+//        public AlbumRepository(IAmbientDbContextLocator dbContextScopeLocator)
+//            : base(dbContextScopeLocator)
+//        {
+//        }
+//    }
 
-    public class ArtistRepository : BaseRepository<Artist, MusicStoreContext>
-    {
-        public ArtistRepository(IAmbientDbContextLocator dbContextScopeLocator)
-            : base(dbContextScopeLocator)
-        {
-        }
-    }
+//    public class ArtistRepository : BaseRepository<Artist, MusicStoreContext>
+//    {
+//        public ArtistRepository(IAmbientDbContextLocator dbContextScopeLocator)
+//            : base(dbContextScopeLocator)
+//        {
+//        }
+//    }
 
-    public class CartRepository : BaseRepository<Cart, MusicStoreContext>
-    {
-        public CartRepository(IAmbientDbContextLocator dbContextScopeLocator)
-            : base(dbContextScopeLocator)
-        {
-        }
-    }
+//    public class CartRepository : BaseRepository<Cart, MusicStoreContext>
+//    {
+//        public CartRepository(IAmbientDbContextLocator dbContextScopeLocator)
+//            : base(dbContextScopeLocator)
+//        {
+//        }
+//    }
 
-    public class CartItemRepository : BaseRepository<CartItem, MusicStoreContext>
-    {
-        public CartItemRepository(IAmbientDbContextLocator dbContextScopeLocator)
-            : base(dbContextScopeLocator)
-        {
-        }
-    }
+//    public class CartItemRepository : BaseRepository<CartItem, MusicStoreContext>
+//    {
+//        public CartItemRepository(IAmbientDbContextLocator dbContextScopeLocator)
+//            : base(dbContextScopeLocator)
+//        {
+//        }
+//    }
 
-    public class GenreRepository : BaseRepository<Genre, MusicStoreContext>
-    {
-        public GenreRepository(IAmbientDbContextLocator dbContextScopeLocator)
-            : base(dbContextScopeLocator)
-        {
-        }
-    }
+//    public class GenreRepository : BaseRepository<Genre, MusicStoreContext>
+//    {
+//        public GenreRepository(IAmbientDbContextLocator dbContextScopeLocator)
+//            : base(dbContextScopeLocator)
+//        {
+//        }
+//    }
 
-    public class OrderRepository : BaseRepository<Order, MusicStoreContext>
-    {
-        public OrderRepository(IAmbientDbContextLocator dbContextScopeLocator)
-            : base(dbContextScopeLocator)
-        {
-        }
-    }
+//    public class OrderRepository : BaseRepository<Order, MusicStoreContext>
+//    {
+//        public OrderRepository(IAmbientDbContextLocator dbContextScopeLocator)
+//            : base(dbContextScopeLocator)
+//        {
+//        }
+//    }
 
-    public class OrderDetailRepository : BaseRepository<OrderDetail, MusicStoreContext>
-    {
-        public OrderDetailRepository(IAmbientDbContextLocator dbContextScopeLocator)
-            : base(dbContextScopeLocator)
-        {
-        }
-    }
-}
+//    public class OrderDetailRepository : BaseRepository<OrderDetail, MusicStoreContext>
+//    {
+//        public OrderDetailRepository(IAmbientDbContextLocator dbContextScopeLocator)
+//            : base(dbContextScopeLocator)
+//        {
+//        }
+//    }
+//}
