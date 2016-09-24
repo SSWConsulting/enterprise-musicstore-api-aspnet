441a1022f0796a93e8f780ca80af317145f18c8e
Igor Goldobin <igor.goldobin@gmail.com>
2016-09-24 17:05:00 +1000

    Removed dataonion.core dependency on API project

--- src/SSW.MusicStore.API/Infrastructure/DI/IoC.cs
+++ src/SSW.MusicStore.API/Infrastructure/DI/IoC.cs
@@ -2,9 +2,9 @@
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SSW.DataOnion.Core;
-using SSW.DataOnion.Core.Initializers;
-using SSW.DataOnion.DependencyResolution.Autofac;
+//using SSW.DataOnion.Core;
+//using SSW.DataOnion.Core.Initializers;
+//using SSW.DataOnion.DependencyResolution.Autofac;
 using SSW.DataOnion.Interfaces;
 using SSW.MusicStore.API.Services;
 using SSW.MusicStore.BusinessLogic.Command;
@@ -28,19 +28,19 @@ namespace SSW.MusicStore.API.Infrastructure.DI
             builder.RegisterAssemblyTypes(typeof (CartCommandService).GetTypeInfo().Assembly, typeof (ICartCommandService).GetTypeInfo().Assembly)
                 .AsImplementedInterfaces();
 
-            var databaseInitializer = new MigrateToLatestVersion(new SampleDataSeeder());
-            builder.AddDataOnion(new DbContextConfig(configuration.GetConnectionString("DefaultConnection"), typeof(MusicStoreContext), databaseInitializer));
+            //var databaseInitializer = new MigrateToLatestVersion(new SampleDataSeeder());
+            //builder.AddDataOnion(new DbContextConfig(configuration.GetConnectionString("DefaultConnection"), typeof(MusicStoreContext), databaseInitializer));
 
             // Populate the container with services that were previously registered
             builder.Populate(services);
 
-            builder.RegisterType<BaseRepository<Album, MusicStoreContext>>().As<IRepository<Album>>();
-            builder.RegisterType<BaseRepository<Artist, MusicStoreContext>>().As<IRepository<Artist>>();
-            builder.RegisterType<BaseRepository<Cart, MusicStoreContext>>().As<IRepository<Cart>>();
-            builder.RegisterType<BaseRepository<CartItem, MusicStoreContext>>().As<IRepository<CartItem>>();
-            builder.RegisterType<BaseRepository<Genre, MusicStoreContext>>().As<IRepository<Genre>>();
-            builder.RegisterType<BaseRepository<Order, MusicStoreContext>>().As<IRepository<Order>>();
-            builder.RegisterType<BaseRepository<OrderDetail, MusicStoreContext>>().As<IRepository<OrderDetail>>();
+            //builder.RegisterType<BaseRepository<Album, MusicStoreContext>>().As<IRepository<Album>>();
+            //builder.RegisterType<BaseRepository<Artist, MusicStoreContext>>().As<IRepository<Artist>>();
+            //builder.RegisterType<BaseRepository<Cart, MusicStoreContext>>().As<IRepository<Cart>>();
+            //builder.RegisterType<BaseRepository<CartItem, MusicStoreContext>>().As<IRepository<CartItem>>();
+            //builder.RegisterType<BaseRepository<Genre, MusicStoreContext>>().As<IRepository<Genre>>();
+            //builder.RegisterType<BaseRepository<Order, MusicStoreContext>>().As<IRepository<Order>>();
+            //builder.RegisterType<BaseRepository<OrderDetail, MusicStoreContext>>().As<IRepository<OrderDetail>>();
 
             var container = builder.Build();
 
